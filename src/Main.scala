95c6331fe43c2e8fbb7d7c7612c3483ccee6c8b1
yuemenglong <yuemenglong@126.com>
2017-06-23 14:40:54 +0800

    add: diff & dup

--- src/Main.scala
+++ src/Main.scala
@@ -1,8 +1,10 @@
-import java.io.{File, FileInputStream}
+import java.io.{File, FileInputStream, PrintWriter}
+import java.nio.file.Paths
 
 import scala.collection.mutable.ArrayBuffer
 import scala.util.control.Breaks._
 import scala.io.Source
+import scala.util.parsing.json.JSONArray
 
 /**
   * Created by Administrator on 2017/6/22.
@@ -18,15 +20,68 @@ header: ca_f_leg_00
  */
 object Main {
   def main(args: Array[String]): Unit = {
-    //    val rows = scan("D:/list/characustom").map(file => {
-    //      println(file)
-    //      parseFile(file)
-    //    }).flatten
-    //    rows.foreach(items => {
-    //      println(items(0), items(2))
+    //    parseFile(new File("D:/list/0/characustom/00.unity3d")).foreach(row => {
+    //      row.foreach(println)
     //    })
-    //    println(rows.length)
-    parseFile(new File("D:\\list\\characustom\\08.unity3d"))
+    findDiff()
+  }
+
+  def findDiff(): Unit = {
+    val r0 = parseDir("D:/list/0/characustom")
+    println(r0.length)
+    val r1 = parseDir("D:/list/1/characustom")
+    println(r1.length)
+
+
+    val writer = new PrintWriter(new File(Paths.get("D:/list/0/characustom", "diff.txt").toString))
+    val s0: Set[String] = r0.map(_._3)(collection.breakOut)
+    r1.foreach(r => {
+      if (!s0.contains(r._3)) {
+        println(r)
+        writer.write(JSONArray(List[String](r._1, r._2, r._3, r._4)).toString())
+        writer.write("\n")
+      }
+    })
+    writer.close()
+  }
+
+  def findDup(): Unit = {
+    val dir = "D:/list/0/characustom"
+    val rows = parseDir(dir)
+    println(rows.length)
+    val writer = new PrintWriter(new File(Paths.get(dir, "diff.txt").toString))
+    var last: (String, String, String, String) = ("", "", "", "")
+    rows.sortBy(_._2).foreach(r => {
+      if (last._2 == r._2 && last._3 != r._3) {
+        println(last)
+        println(r)
+        println()
+        writer.write(JSONArray(List[String](last._1, last._2, last._3, last._4)).toString())
+        writer.write("\n")
+        writer.write(JSONArray(List[String](r._1, r._2, r._3, r._4)).toString())
+        writer.write("\n")
+        writer.write("\n")
+      }
+      last = r
+    })
+    writer.close()
+  }
+
+  def parseDir(dir: String): List[(String, String, String, String)] = {
+    val rows = listFiles(dir).flatMap(file => {
+      println(file)
+      parseFile(file).map(row => {
+        (file.getPath, row(0), row(2), row(4))
+      })
+    })
+    val file = new File(Paths.get(dir, "list.txt").toString)
+    val writer = new PrintWriter(file)
+    rows.foreach(row => {
+      writer.write(JSONArray(List[String](row._1, row._2, row._3, row._4)).toString())
+      writer.write("\n")
+    })
+    writer.close()
+    rows
   }
 
   def splitBy(buffer: Array[Byte], flag: Byte): Array[Array[Byte]] = {
@@ -64,9 +119,9 @@ object Main {
   def printAble(b: Byte): Boolean = {
     if ('a' <= b && b <= 'z' || 'A' <= b && b <= 'Z' || '0' <= b && b <= '9'
       || "_.".indexOf(b) >= 0) {
-      return true
+      true
     } else {
-      return false
+      false
     }
   }
 
@@ -90,7 +145,6 @@ object Main {
           case (-1, b) => b
           case (a, b) => Math.min(a, b)
         }
-        println("%X, %X, %X".format(start, end, pos))
         val line = buffer.drop(start).take(end - start)
         val items = splitBy(line, 0x09)
         if (items.length > 1) {
@@ -100,19 +154,19 @@ object Main {
         pos = end
       }
     }
-    return rows.toArray
+    rows.toArray
   }
 
-  def scan(path: String): List[File] = {
-    scan(new File(path))
+  def listFiles(path: String): List[File] = {
+    listFiles(new File(path))
   }
 
-  def scan(file: File): List[File] = {
+  def listFiles(file: File): List[File] = {
     if (file.getName.endsWith(".unity3d")) {
       return List(file)
     }
     if (file.isDirectory) {
-      file.listFiles().map(scan).flatten.toList
+      file.listFiles().map(listFiles).flatten.toList
     } else {
       List()
     }
